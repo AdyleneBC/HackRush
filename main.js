994bfda6e7a1f3e32d124f32d6c16acb9886d21a
FernandoRuizV <lasleguminosas@gmail.com>
2025-09-11 08:47:28 -0600

    Ultimos cambios

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 // Archivo: main.js
+import { EscenaInicio } from './EscenaInicio.js';
+import { IntroScene } from './IntroScene.js';
+import { Menu } from './menu.js';
 import { Escena, Mazmorra1 } from './Escenas.js';
+
 
 const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
-  scene: [Escena,Mazmorra1], // Añade tu escena al array
+  scene: [EscenaInicio, IntroScene, Menu, Escena, Mazmorra1], // Añade tu escena al array
   physics: {
     default: 'arcade',
     arcade: {
