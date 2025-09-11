5a8ee7549d5c74a0d06ac258fd00e0c19210d97d
FernandoRuizV <lasleguminosas@gmail.com>
2025-09-11 01:39:35 -0600

    Creacion de carpetas

---
+++ codigo/Clases.js
@@ -0,0 +1,53 @@
+export class Personaje {
+  constructor(nivel, vida) {
+    this.nombre;
+    this.nivel = nivel;
+    this.vida = vida;
+    this.medallas;
+    this.ataque;
+    this.defensa=0;
+  }
+  subirNivel() {
+    this.nivel += 1;
+    console.log(`${this.nombre} ha subido al nivel ${this.nivel}!`);
+  }
+  atacar(Objetivo){
+    Objetivo.recibirDanio(this.ataque);
+    console.log(`${this.nombre} ataca a ${Objetivo.nombre}`);
+  }
+  defenderse() {
+    this.defensa+=10;
+    console.log(`${this.nombre} ha aumentado su defensa en ${this.defensa}`);
+  }
+  recibirDanio(cantidad) {
+    let danio= cantidad;
+    if (this.defensa>0 && this.defensa<danio){
+        danio-=this.defensa;
+        this.vida -= danio;
+    } else {
+        this.vida-=danio
+        if(this.vida<=0){
+            console.log(`${this.nombre} ha sido derrotado.`);
+        }
+    }
+  }
+
+  mostrarEstado() {
+    console.log(`--- Estado del Personaje ---`);
+    console.log(`Nombre: ${this.nombre}`);
+    console.log(`Nivel: ${this.nivel}`);
+    console.log(`Vida: ${this.vida}`);
+    console.log(`Medallas: ${this.medallas}`);
+    console.log(`----------------------------`);
+  }
+}
+
+export class Enemigo {
+    constructor(nombre, vida, tipo, ataque, debilidad) {
+        this.nombre=nombre;
+        this.vida=vida;
+        this.tipo= tipo;
+        this.ataque=ataque;
+        this.debilidad=debilidad
+    }
+}
---
+++ codigo/main.js
@@ -0,0 +1,18 @@
+// Archivo: main.js
+import { Escena } from './Escenas.js';
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: [Escena], // Añade tu escena al array
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 }, // Establece la gravedad a 0 para que el personaje no caiga
+      debug: false // Cambia a true para ver los cuerpos de las físicas
+    }
+  }
+};
+
+const game = new Phaser.Game(config);
